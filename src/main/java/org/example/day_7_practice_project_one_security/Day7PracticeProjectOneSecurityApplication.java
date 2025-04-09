630c2cbff200eb8d36840c272645e9df08e84540
evgen <zekanov16@gmail.com>
2025-04-09 22:40:26 +0300

    first commit

---
+++ src/main/java/org/example/day_7_practice_project_one_security/Day7PracticeProjectOneSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.day_7_practice_project_one_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day7PracticeProjectOneSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day7PracticeProjectOneSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/day_7_practice_project_one_security/Day7PracticeProjectOneSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day_7_practice_project_one_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day7PracticeProjectOneSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
